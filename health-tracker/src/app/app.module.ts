16f86927e537dcc3f4bd27ab5e584e23d7a11fe2
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-07-16 11:44:29 +0000

    workout-form added

--- health-tracker/src/app/app.module.ts
+++ health-tracker/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkoutFormComponent } from './workout-form/workout-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkoutFormComponent
   ],
   imports: [
     BrowserModule,
