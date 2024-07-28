2ceab307ff031827780803e8be3456341a302e3c
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-07-28 17:23:26 +0000

    health tracker initiated

--- health-tracker/src/app/app.module.ts
+++ health-tracker/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WorkoutFormComponent } from './workout-form/workout-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WorkoutFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
